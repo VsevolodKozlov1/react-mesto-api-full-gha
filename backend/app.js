e413d2f84bc1a864b98935f586c312fa03178b23
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-11 20:44:05 +0300

    fix: cors 2

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,15 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cors = require('cors');
 const limiter = require('./utils/limiter-config');
 const routes = require('./routes');
+const cors = require('./middlewares/cors');
 const errHandler = require('./middlewares/err-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
-app.use(cors());
-app.options('*', cors());
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(helmet());
@@ -22,6 +20,7 @@ app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(cors);
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
