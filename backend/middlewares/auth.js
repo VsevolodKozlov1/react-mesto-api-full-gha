d1d554ecbaf390b4e66f4e167e2dc65112263393
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-12 17:14:23 +0300

    fix: env 3

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,14 +3,14 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-
+  const { NODE_ENV, JWT_SECRET } = process.env;
   if (!token) {
     throw new UnauthorizedError('Вход в аккаунт не выполнен');
   }
 
   let payload;
   try {
-    payload = jwt.verify(token, 'some-secret-key');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     throw new UnauthorizedError('Вход в аккаунт не выполнен');
   }
