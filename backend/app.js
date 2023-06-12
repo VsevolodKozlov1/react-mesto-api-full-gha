b5bf517a402b71b8dcc01577f13d1ecb9de17d9a
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-12 08:14:17 +0300

    fix: cors 7

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,9 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-// const cors = require('cors');
 const limiter = require('./utils/limiter-config');
 const routes = require('./routes');
+const cors = require('./middlewares/cors');
 const errHandler = require('./middlewares/err-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 // require('dotenv').config();
@@ -21,13 +21,7 @@ app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-// app.use(cors({
-//   origin: [
-//     'https://mesto-vsevolodk.nomoredomains.rocks',
-//     'http://mesto-vsevolodk.nomoredomains.rocks',
-//   ],
-// }));
-
+app.use(cors);
 app.use(requestLogger);
 app.use(routes);
 app.use(errorLogger);
