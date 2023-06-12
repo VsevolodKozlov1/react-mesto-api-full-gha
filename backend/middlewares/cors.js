64e5514a707fb67f28f8981adc1cc4f9c615464f
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-12 12:47:18 +0300

    fix: cors 8

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,19 +1,21 @@
 const allowedCors = [
-  'http://mesto-vsevolodk.nomoredomains.rocks/signin',
+  'http://mesto-vsevolodk.nomoredomains.rocks',
   'https://mesto-vsevolodk.nomoredomains.rocks',
   'localhost:3000',
 ];
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept';
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+      // return res.end();
+    }
   }
-
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-  // res.header('Access-Control-Allow-Origin', '*');
   next();
 };
