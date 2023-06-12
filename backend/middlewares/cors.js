62d77b7b7053e444e4533204c276ad23404d2fbd
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-12 12:58:13 +0300

    fix: cors 10

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,19 +1,21 @@
-const allowedCors = [
-  'https://mesto-vsevolodk.nomoredomains.rocks',
-];
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+// const allowedCors = [
+//   'https://mesto-vsevolodk.nomoredomains.rocks',
+// ];
+// const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 // const DEFAULT_ALLOWED_HEADERS = 'Origin, X-Requested-With, Content-Type, Accept';
 
 module.exports = (req, res, next) => {
-  const { origin } = req.headers;
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-      res.header('Access-Control-Allow-Headers', requestHeaders);
-      // return res.end();
-    }
-  }
+  // const { origin } = req.headers;
+  // const requestHeaders = req.headers['access-control-request-headers'];
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  //   if (req.method === 'OPTIONS') {
+  //     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+  //     res.header('Access-Control-Allow-Headers', requestHeaders);
+  //   }
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', '*');
+  res.header('Access-Control-Allow-Headers', '*');
   next();
 };
