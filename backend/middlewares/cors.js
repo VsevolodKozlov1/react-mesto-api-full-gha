3b78decbec40c2f317d4749a1923fd0bb113dd99
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-13 09:35:35 +0300

    fix: cookies in response 11

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', 'false');
+  res.header('Access-Control-Allow-Origin', 'https://mesto-vsevolodk.nomoredomains.rocks');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
