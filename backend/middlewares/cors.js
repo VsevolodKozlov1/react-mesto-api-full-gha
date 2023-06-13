e5970680479ed520ad1d7d79f3ee1c2e630fc488
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-13 09:33:17 +0300

    fix: cookies in response 10

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'false');
   if (req.method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', '*');
     res.header('Access-Control-Allow-Headers', '*');
