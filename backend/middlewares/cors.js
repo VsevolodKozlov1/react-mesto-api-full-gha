a01c78dd1243d97940acdb5daf5f8f60aee7b122
Vsevolod Kozlov <skifff1993@yandex.ru>
2023-06-13 09:40:17 +0300

    fix: cookies in response 13

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -3,8 +3,8 @@ module.exports = (req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://mesto-vsevolodk.nomoredomains.rocks');
   res.header('Access-Control-Allow-Credentials', 'true');
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', '*');
-    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-type');
     return res.end();
   }
   next();
